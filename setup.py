2e6dd7e334291f59f8788d1f4184a16c392d36ca
liddyjacob <liddyjacob@gmail.com>
2022-02-15 23:02:06 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-import sqlite3
+from models import List, Person, database
 
-# DB-API spec for talking to relational databases in Python
-
-connection = sqlite3.connect("shopping_list.db")
-
-cursor = connection.cursor()
-
-# cursor.execute("drop table list")
-
-cursor.execute("create table list (id integer primary key, description text)")
-
-cursor.execute("insert into list (description) values ('apples')")
-cursor.execute("insert into list (description) values ('broccoli')")
-cursor.execute("insert into list (description) values ('pizza')")
-cursor.execute("insert into list (description) values ('tangerine')")
-cursor.execute("insert into list (description) values ('potatoes')")
-
-connection.commit()
-connection.close()
-
-print("done.")
-
+database.create_tables([List, Person])
